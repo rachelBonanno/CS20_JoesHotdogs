299a94d4c11f6ae1a01a7f2dbf0970cd659df8fd
rachelBonanno <rachel.bonanno@tufts.edu>
2022-10-03 12:42:55 -0400

    total, discount, tax

--- order.js
+++ order.js
@@ -5,28 +5,47 @@ function amount() {
     var hotdog = prompt("How many hotdogs would you like?");
     var fries = prompt("How many fries would you like?");
     var drinks = prompt("How many drinks would you like?");
+    var hp;
+    var fp;
+    var dp;
     if (hotdog == null || hotdog == "") {
+        hp = 0;
         document.getElementById("hotdogsprice").innerHTML = 0 + " Hotdogs: $" + 0;
     } else {
-        document.getElementById("hotdogsprice").innerHTML = hotdog + " Hotdogs: $" + (hotdog * hotdogprice);
+        hp = hotdog * hotdogprice;
+        document.getElementById("hotdogsprice").innerHTML = hotdog + " Hotdogs: $" + hp.toFixed(2);
     }
     if (fries == null || fries == "") {
+        fp = 0;
         document.getElementById("friesprice").innerHTML = 0 + " Fries: $" + 0;
     } else {
-        document.getElementById("friesprice").innerHTML = fries + " Fries: $" + (fries * friesprice);
+        fp = fries * friesprice;
+        document.getElementById("friesprice").innerHTML = fries + " Fries: $" + fp.toFixed(2);
     }
     if (drinks == null || drinks == "") {
+        dp = 0;
         document.getElementById("drinksprice").innerHTML = 0 + " Drinks: $" + 0;
     } else {
-        document.getElementById("drinksprice").innerHTML = drinks + " Drinks: $" + (drinks * drinksprice);
+        dp = drinks * drinksprice;
+        document.getElementById("drinksprice").innerHTML = drinks + " Drinks: $" + dp.toFixed(2);
     }
-    var total = (hotdogprice + friesprice + drinksprice);
+    var total = (hp + fp + dp);
+    var tax;
+    var grand;
     if (total >= 20) {
-        document.getElementById("text").innerHTML = "Total (With 10% discount): $" + (total * 0.1);
+        var discount = total * 0.9;
+        document.getElementById("text").innerHTML = "Total (With 10% discount): $" + discount.toFixed(2);
+        tax = discount * 0.0625;
+        document.getElementById("tax").innerHTML = "6.25% Tax: $" + tax.toFixed(2);
+        grand = discount + tax;
+        document.getElementById("total").innerHTML = "Total + Tax: $" + grand.toFixed(2);
     } else {
-
+        document.getElementById("text").innerHTML = "Total: $" + total.toFixed(2);
+        tax = total * 0.0625;
+        document.getElementById("tax").innerHTML = "6.25% Tax: $" + tax.toFixed(2);
+        grand = total + tax;
+        document.getElementById("total").innerHTML = "Total + Tax: $" + grand.toFixed(2);
     }
-    document.getElementById("text").innerHTML = "Total: $" + total;
 }
 
 
